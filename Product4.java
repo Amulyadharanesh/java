bec2f02cac9a28d3192c92a0d663bc7044b60882
Amulyadharanesh <amulyadharaneshxworkz.gmail.com>
2023-12-04 13:38:25 +0530

    boolean

---
+++ Product4.java
@@ -0,0 +1,95 @@
+public class Product4{
+public static void main(String args[])
+{
+	boolean isCoffeeTasteGood= true;
+	System.out.println(isCoffeeTasteGood);
+	
+	isCoffeeTasteGood= false;
+	System.out.println(isCoffeeTasteGood);
+	
+	boolean isGoingToCoolegeIsFUn= true;
+	System.out.println(isGoingToCoolegeIsFUn);
+	
+	isGoingToCoolegeIsFUn= false;
+	System.out.println(isGoingToCoolegeIsFUn);
+	
+	boolean ismyPaintingIsGood= true;
+	System.out.println(ismyPaintingIsGood);
+	
+	ismyPaintingIsGood= false;
+	System.out.println(ismyPaintingIsGood);
+	
+	boolean isMentalHeaalthIsImportant= true;
+	System.out.println(isMentalHeaalthIsImportant);
+	
+	
+	boolean isSaiPallaviIsABeautifullActress= true;
+	System.out.println(isSaiPallaviIsABeautifullActress);
+	
+	isSaiPallaviIsABeautifullActress=false;
+	System.out.println(isSaiPallaviIsABeautifullActress);
+	
+	boolean cricketIsAIntrestingGame=true;
+	System.out.println(cricketIsAIntrestingGame);
+	
+	cricketIsAIntrestingGame=false;
+	System.out.println(cricketIsAIntrestingGame);
+	
+	boolean isBeingHomeFeelsGood=true;
+	System.out.println(isBeingHomeFeelsGood);
+	
+	boolean isPlayingBadmitonIsRelaxing=true;
+	System.out.println(isPlayingBadmitonIsRelaxing);
+	
+	isPlayingBadmitonIsRelaxing=false;
+	System.out.println(isPlayingBadmitonIsRelaxing);
+	
+	boolean isMusicHealsEverything=true;
+	System.out.println(isMusicHealsEverything);
+	
+	isMusicHealsEverything=false;
+	System.out.println(isMusicHealsEverything);
+	
+	boolean doesWinterFeelsGood=true;
+	System.out.println(doesWinterFeelsGood);
+	
+	doesWinterFeelsGood=false;
+	System.out.println(doesWinterFeelsGood);
+	
+	boolean isKannadaIsAEasyLanguage=true;
+	System.out.println(isKannadaIsAEasyLanguage);
+	
+	isKannadaIsAEasyLanguage=false;
+	System.out.println(isKannadaIsAEasyLanguage);
+	
+	boolean isReadingNovelFeelsGood=true;
+	System.out.println(isReadingNovelFeelsGood);
+	
+	isReadingNovelFeelsGood=false;
+	System.out.println(isReadingNovelFeelsGood);
+	
+	boolean isBeingAFoodieIsGood=true;
+	System.out.println(isBeingAFoodieIsGood);
+	
+	boolean isRidingBikeIsSafe=true;
+	System.out.println(isRidingBikeIsSafe);
+	
+	isRidingBikeIsSafe=false;
+	System.out.println(isRidingBikeIsSafe);
+	
+	boolean isBuyingClothesIsHappiness=true;
+	System.out.println(isBuyingClothesIsHappiness);
+	
+	boolean doesOrdinaryWorthTheHype=true;
+	System.out.println(doesOrdinaryWorthTheHype);
+	
+	doesOrdinaryWorthTheHype=false;
+	System.out.println(doesOrdinaryWorthTheHype);
+	
+	boolean doesTravelingMakesYouHappy=true;
+	System.out.println(doesTravelingMakesYouHappy);
+}
+}
+	
+	
+	
