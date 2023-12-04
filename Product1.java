7d74c5cb8e2874df5210d65bae53c8a577716fda
Amulyadharanesh <amulyadharaneshxworkz.gmail.com>
2023-12-04 21:13:50 +0530

    float

---
+++ Product1.java
@@ -0,0 +1,118 @@
+public class Product1{
+public static void main(String args[])
+{
+	float temperatureOfBengaluru=23.5f;
+	System.out.println(temperatureOfBengaluru);
+	
+	temperatureOfBengaluru=25.4f;
+	System.out.println(temperatureOfBengaluru);
+	
+	float currencyOftenRupeeInDollars=0.12000f;
+	System.out.println(currencyOftenRupeeInDollars);
+	
+	currencyOftenRupeeInDollars=0.13000f;
+	System.out.println(currencyOftenRupeeInDollars);
+	
+	float bohrRadiusConstant=0.529f;
+	System.out.println(bohrRadiusConstant);
+	
+	float wattOfBulb=0.06f;
+	System.out.println(wattOfBulb);
+	
+	wattOfBulb=0.05f;
+	System.out.println(wattOfBulb);
+	
+	float sizeOfShoe=6.5f;
+	System.out.println(sizeOfShoe);
+	
+	sizeOfShoe=7.5f;
+	System.out.println(sizeOfShoe);
+	
+	float currentIncreaseStockRateOfAppleCompany=0.68f;
+	System.out.println(currentIncreaseStockRateOfAppleCompany);
+	
+	currentIncreaseStockRateOfAppleCompany=0.80f;
+	System.out.println(currentIncreaseStockRateOfAppleCompany);
+	
+	float heightOfAPerson=5.4f;
+	System.out.println(heightOfAPerson);
+	
+	heightOfAPerson=5.8f;
+	System.out.println(heightOfAPerson);
+	 
+	float weightOfPerson=59.8f;
+	System.out.println(weightOfPerson);
+	
+	weightOfPerson=61.8f;
+	System.out.println(weightOfPerson);
+	
+	float marksOfSemster=6.5f;
+	System.out.println( marksOfSemster);
+	
+	marksOfSemster=6.8f;
+	System.out.println( marksOfSemster);
+	
+    float gasConstant=8.314f;
+	System.out.println(gasConstant);
+	
+	float avogadrosNumberConstant=6.022f;
+	System.out.println(avogadrosNumberConstant);
+	
+	float chargeOfElectron=1.602f;
+	System.out.println(chargeOfElectron);
+	
+	float currentTimeOfIndia=2.30f;
+	System.out.println(currentTimeOfIndia);
+	
+	currentTimeOfIndia=3.30f;
+	System.out.println(currentTimeOfIndia);
+	
+	float priceOFShampoo=1.80f;
+	System.out.println(priceOFShampoo);
+	
+	priceOFShampoo=2.80f;
+	System.out.println(priceOFShampoo);
+	
+	float constantOfPi=3.147f;
+	System.out.println(constantOfPi);
+	
+	float electricityRatePerUnit=4.75f;
+	System.out.println(electricityRatePerUnit);
+	
+	electricityRatePerUnit=4.85f;
+	System.out.println(electricityRatePerUnit);
+	
+	float lengthOfPaper=8.5f;
+	System.out.println(lengthOfPaper);
+	
+	lengthOfPaper=8.75f;
+	System.out.println(lengthOfPaper);
+	
+	float instaFollowers=1.070f;
+	System.out.println(instaFollowers);
+	
+	instaFollowers=1.070f;
+	System.out.println(instaFollowers);
+	
+	float strikeRateOfViratKholiInOdi=50.44f;
+	System.out.println(strikeRateOfViratKholiInOdi);
+	
+	strikeRateOfViratKholiInOdi=51.44f;
+	System.out.println(strikeRateOfViratKholiInOdi);
+				
+}
+}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+		
