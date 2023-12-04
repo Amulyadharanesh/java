714dbdb1ffc796af7e3903d5877576edcabdd67f
Amulyadharanesh <amulyadharaneshxworkz.gmail.com>
2023-12-04 13:51:28 +0530

    char

---
+++ Product3.java
@@ -0,0 +1,89 @@
+public class Product3{
+public static void main(String args[])
+{
+	char firstLetterOfConsonants='b';
+	System.out.println(firstLetterOfConsonants);
+	
+	char lastLetterOfVowels='u';
+	System.out.println(lastLetterOfVowels);
+	
+	char firstLetterOfVowels='a';
+	System.out.println(firstLetterOfVowels);
+	
+	char lastLetterOfConsonants='z';
+	System.out.println(lastLetterOfConsonants);
+	
+	char resultOfSem='P';
+	System.out.println(resultOfSem);
+	
+	resultOfSem='F';
+	System.out.println(resultOfSem);
+	
+	char gradeOfResult='S';
+	System.out.println(gradeOfResult);
+	
+	gradeOfResult='C';
+	System.out.println(gradeOfResult);
+	
+	char symbolOfPotassium='K';
+	System.out.println(symbolOfPotassium);
+	
+	char symbolOfHydrogen='H';
+	System.out.println(symbolOfHydrogen);
+	
+	char typeOfPortUsedForAndroid='C';
+	System.out.println(typeOfPortUsedForAndroid);
+	
+	typeOfPortUsedForAndroid='B';
+	System.out.println(typeOfPortUsedForAndroid);
+	
+	char symbolOfNitrogen='N';
+	System.out.println(symbolOfNitrogen);
+	
+	char symbolOfOxygen='O';
+	System.out.println(symbolOfOxygen);
+	
+	char symbolOfSulphur='S';
+	System.out.println(symbolOfSulphur);
+	
+	char sizeOfCloth='S';
+	System.out.println(sizeOfCloth);
+	
+	sizeOfCloth='M';
+	System.out.println(sizeOfCloth);
+	
+	char typesOfGender='F';
+	System.out.println(typesOfGender);
+	
+	typesOfGender='M';
+	System.out.println(typesOfGender);
+	
+	char startingWordOfAmulya='A';
+	System.out.println(startingWordOfAmulya);
+	
+	char endingWordOfAmulya='a';
+	System.out.println(endingWordOfAmulya);
+	
+	char startingWordOfCoffee='C';
+	System.out.println(startingWordOfCoffee);
+	
+	char measuringScale='m';
+	System.out.println(measuringScale);
+	
+	measuringScale='a';
+	System.out.println(measuringScale);
+}
+}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
