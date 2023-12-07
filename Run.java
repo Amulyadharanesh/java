629fa35513d83af9b3b3704fa7279e815ebf144b
Amulyadharanesh <amulyadharaneshxworkz.gmail.com>
2023-12-07 23:05:25 +0530

    method

---
+++ Run.java
@@ -0,0 +1,115 @@
+public class Run{
+	public static void main(String[] args){
+
+int returnedValue=sub();
+System.out.println(returnedValue);
+
+byte returnNumber=write();
+System.out.println(returnedValue);
+
+long returnedCreation=watch();
+System.out.println(returnedValue);
+
+char returnType=walk();
+System.out.println(returnType);
+
+boolean returnedType=read();
+System.out.println(returnedType);
+
+float returnedNumber =sum();
+System.out.println(returnedNumber);
+
+double returnedWord=word();
+System.out.println(returnedWord);
+
+short returnedMultiple=multi();
+System.out.println(returnedMultiple);
+
+}
+
+static int sub()
+{
+	int num1=40;
+	int num2=25;
+	int value=num1-num2;
+	return value;
+}
+
+static byte write()
+{
+	byte number=30;
+	return number;
+}
+
+static long watch()
+{
+	long value1=656632L;
+	long value2=787322L;
+	long value=value1+value2;
+	return value;
+}
+
+static char walk()
+{
+	char type='a';
+	return type;
+}
+
+static boolean read()
+{
+	boolean value=true;
+	return value;
+}
+
+static float sum()
+{
+	float val1=7.8f;
+	float val2=4.6f;
+	float value=val1*val2;
+	return value;
+}
+
+static double word()
+{
+	double value=9.4563536d;
+	return value;
+}
+
+static short multi()
+{
+	short value=645;
+	return value;
+}
+}
+	
+
+
+
+
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
