88f93f5a6b515ef23773a7cb2bbda3c2265712bc
Amulyadharanesh <amulyadharaneshxworkz.gmail.com>
2023-12-11 22:26:10 +0530

    parameter

---
+++ Computer.java
@@ -0,0 +1,62 @@
+public class Computer{
+	public static void main(String[] args){
+		keyword(3455);
+		marksOfSemster(6.45f);
+		keyword(100);
+	    phoneNumber(9538287479L);
+		keyword(2000);
+        keplerBouwkampValue(0.1149420448532962007D);		
+		resultOfSem('P');	
+        isCoffeeTasteGood(true);
+
+	}
+	
+ static void keyword(int input)
+ {
+System.out.println("input value is " + input);	 
+ }
+
+static void marksOfSemster(float input)
+{
+System.out.println("input value is " + input);
+}
+
+static void keyword(byte input)
+{
+System.out.println("input value is " + input);
+}
+
+static void  phoneNumber(long input)
+{
+System.out.println("input value is " + input);
+}
+
+static void keyword(short input)
+{
+System.out.println("input value is " + input);
+}
+
+static void keplerBouwkampValue(double input)
+{
+System.out.println("input value is " + input);
+}
+
+static void resultOfSem(char input)
+{
+System.out.println("input value is " + input);
+}
+
+static void isCoffeeTasteGood(boolean input)
+{
+System.out.println("input value is " + input);
+}
+}
+
+
+
+ 	
+	
+	
+
+
+	
