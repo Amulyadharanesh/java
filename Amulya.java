2dade8afb14eed7a05361b281cb8e45d883a4aa1
Amulyadharanesh <amulyadharaneshxworkz.gmail.com>
2023-12-13 23:02:29 +0530

    for loop

---
+++ Amulya.java
@@ -0,0 +1,51 @@
+public class Amulya {
+    public static void main(String[] args) {
+        int[] numbers = {13, 67, 345, 786, 456, 677, 234, 34, 67, 898, 345, 230, 98, 78, 67, 34, 21, 65, 78, 67};
+
+        for (int i = 0; i < 20; i++) {
+            System.out.println(numbers[i]);
+        }
+
+        float[] floatNumbers = {45.6f, 56.89f, 89.85f, 45.56f, 89.78f, 75.56f, 89.76f, 65.67f, 99.56f, 34.67f, 28.89f, 86.87f, 33.67f, 11.56f, 27.86f, 67.98f, 786.56f, 685.78f, 943.67f, 765.89f};
+
+        for (int i = 0; i < 20; i++) {
+            System.out.println(floatNumbers[i]);
+        }
+
+        short[] shortArray = {(short) 89, (short) 99, (short) 785, (short) 765, (short) 875, (short) 567, (short) 465, (short) 474, (short) 345, (short) 464, (short) 78, (short) 474, (short) 465, (short) 65, (short) 287, (short) 111, (short) 456, (short) 765, (short) 785, (short) 812};
+
+        for (int i = 0; i < 20; i++) {
+            System.out.println(shortArray[i]);
+        }
+
+        byte[] values = {(byte) 11, (byte) 12, (byte) 13, (byte) 14, (byte) 15, (byte) 16, (byte) 17, (byte) 18, (byte) 19, (byte) 20, (byte) 21, (byte) 22, (byte) 23, (byte) 24, (byte) 25, (byte) 26, (byte) 27, (byte) 28, (byte) 29, (byte) 30};
+
+        for (int i = 0; i < 20; i++) {
+            System.out.println(values[i]);
+        }
+
+        char[] charValues = {'a', 'm', 'u', 'l', 'y', 'a', 'b', 'c', 'd', 'e', 'q', 'w', 'r', 't', 'y', 'u', 'i', 'o', 'l', 'k'};
+
+        for (int i = 0; i < 20; i++) {
+            System.out.println(charValues[i]);
+        }
+
+        long[] longArray = {9538287479L, 8932732772L, 7843839547L, 9993638227L, 9538287579L, 8932732872L, 7843639547L, 9993638527L, 6362467845L, 7996386089L, 1234567891L, 2345678912L, 3456789012L, 4556778901L, 5678901234L, 6789012345L, 7890123456L, 8901234567L, 9012345678L, 8012345672L};
+
+        for (int i = 0; i < 20; i++) {
+            System.out.println(longArray[i]);
+        }
+
+        double[] doubleValues = {95382874d, 89327327d, 78438395d, 999363822d, 95382875d, 893273287d, 784363954d, 99936385d, 636246784d, 799638608d, 123456789d, 234567891d, 345678901d, 4556778901d, 567890123d, 678901234d, 789012345d, 89012345d, 901234566d, 80123456d};
+
+        for (int i = 0; i < 20; i++) {
+            System.out.println(doubleValues[i]);
+        }
+
+        boolean[] booleanArray = {true, false, true, false, true, false, true, false, true, false, true, false, true, false, true, false, true, false, true, false};
+
+        for (int i = 0; i < 20; i++) {
+            System.out.println(booleanArray[i]);
+        }
+    }
+}
